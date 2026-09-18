8cc9de920105714a6da623bd782e9fd7ccb53937
yuuuh26 <dengana.10011212@gmail.com>
2026-09-18 13:39:18 +0900

    Combine the 100 additional TOEIC Part 2 questions

---
+++ questions-extra.js
@@ -0,0 +1,13 @@
+import { EXTRA_QUESTIONS_1 } from "./questions-extra-1.js";
+import { EXTRA_QUESTIONS_2 } from "./questions-extra-2.js";
+import { EXTRA_QUESTIONS_3 } from "./questions-extra-3.js";
+import { EXTRA_QUESTIONS_4 } from "./questions-extra-4.js";
+import { EXTRA_QUESTIONS_5 } from "./questions-extra-5.js";
+
+export const EXTRA_QUESTIONS = [
+  ...EXTRA_QUESTIONS_1,
+  ...EXTRA_QUESTIONS_2,
+  ...EXTRA_QUESTIONS_3,
+  ...EXTRA_QUESTIONS_4,
+  ...EXTRA_QUESTIONS_5
+];
