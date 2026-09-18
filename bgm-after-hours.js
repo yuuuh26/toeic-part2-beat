27a301b81df3bfb23a6f9ea0f1401ebc3cabfbdf
yuuuh26 <dengana.10011212@gmail.com>
2026-09-18 13:42:31 +0900

    Assemble compact After Hours Velocity BGM

---
+++ bgm-after-hours.js
@@ -0,0 +1,4 @@
+import { BGM_SMALL_1 } from "./bgm-small-1.js";
+import { BGM_SMALL_2 } from "./bgm-small-2.js";
+
+export const AFTER_HOURS_VELOCITY_BGM = "data:audio/mpeg;base64," + BGM_SMALL_1 + BGM_SMALL_2;
